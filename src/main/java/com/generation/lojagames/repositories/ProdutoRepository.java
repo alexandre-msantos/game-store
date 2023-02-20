56bfc97a3c980ed6e5a4612aebd3c014e0a5870e
AlexandreMarcelino <alexandre.msantos@mercadolivre.com>
2023-02-20 16:51:30 -0300

    Produto repository implementado

---
+++ src/main/java/com/generation/lojagames/repositories/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.generation.lojagames.repositories;
+
+import com.generation.lojagames.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+}
