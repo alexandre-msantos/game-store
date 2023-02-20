5a135f6f9fe22ccfda48a31fadc91a1012790c4a
AlexandreMarcelino <alexandre.msantos@mercadolivre.com>
2023-02-20 12:29:37 -0300

    Categoria repository implementado

---
+++ src/main/java/com/generation/lojagames/repositories/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.generation.lojagames.repositories;
+
+import com.generation.lojagames.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+}
