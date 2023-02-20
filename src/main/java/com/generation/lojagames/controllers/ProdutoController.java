b46c1bd9d7f06b197170fbab5f757c6feaa2b97a
AlexandreMarcelino <alexandre.msantos@mercadolivre.com>
2023-02-20 17:32:05 -0300

    Metodo findAll implementado

--- src/main/java/com/generation/lojagames/controllers/ProdutoController.java
+++ src/main/java/com/generation/lojagames/controllers/ProdutoController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/produtos")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -16,9 +18,16 @@ public class ProdutoController {
     @Autowired
     private ProdutoRepository produtoRepository;
 
+    @GetMapping
+    public ResponseEntity<List<Produto>> findAll(){
+        return ResponseEntity.ok().body(produtoRepository.findAll());
+    }
+
     @PostMapping
     public ResponseEntity<Produto> insert(@RequestBody Produto produto){
         return ResponseEntity.status(HttpStatus.CREATED).body(produtoRepository.save(produto));
     }
+
+
 
 }
