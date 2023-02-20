6d7647b2892b791726c036870e5ff6ff85c5ceb6
AlexandreMarcelino <alexandre.msantos@mercadolivre.com>
2023-02-20 13:29:51 -0300

    Metodo para  buscar por id implementado

--- src/main/java/com/generation/lojagames/controllers/CategoriaController.java
+++ src/main/java/com/generation/lojagames/controllers/CategoriaController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/categorias")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -14,6 +16,18 @@ public class CategoriaController {
 
     @Autowired
     private CategoriaRepository categoriaRepository;
+
+    @GetMapping
+    private ResponseEntity<List<Categoria>> findAll(){
+        return ResponseEntity.ok().body(categoriaRepository.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    private ResponseEntity<Categoria> findById(@PathVariable Long id){
+        return categoriaRepository.findById(id)
+                .map(obj -> ResponseEntity.ok(obj))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
 
     @PostMapping
     public ResponseEntity<Categoria> insert(@RequestBody Categoria categoria){
