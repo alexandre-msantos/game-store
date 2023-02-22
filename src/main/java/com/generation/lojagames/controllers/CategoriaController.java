9b2d5bfb4a458d5d5d6c95c58e2772c58e5604a7
AlexandreMarcelino <alexandre.msantos@mercadolivre.com>
2023-02-22 11:30:41 -0300

    Validador adicionado aos metodos insert e update

--- src/main/java/com/generation/lojagames/controllers/CategoriaController.java
+++ src/main/java/com/generation/lojagames/controllers/CategoriaController.java
@@ -2,6 +2,7 @@ package com.generation.lojagames.controllers;
 
 import com.generation.lojagames.model.Categoria;
 import com.generation.lojagames.repositories.CategoriaRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -36,12 +37,12 @@ public class CategoriaController {
     }
 
     @PostMapping
-    public ResponseEntity<Categoria> insert(@RequestBody Categoria categoria){
+    public ResponseEntity<Categoria> insert(@Valid @RequestBody Categoria categoria){
         return ResponseEntity.status(HttpStatus.CREATED).body(categoriaRepository.save(categoria));
     }
 
     @PutMapping
-    public ResponseEntity<Categoria> update(@RequestBody Categoria categoria){
+    public ResponseEntity<Categoria> update(@Valid @RequestBody Categoria categoria){
         return categoriaRepository.findById(categoria.getId())
                 .map(obj -> ResponseEntity.ok().body(categoriaRepository.save(categoria)))
                 .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
--- src/main/java/com/generation/lojagames/controllers/ProdutoController.java
+++ src/main/java/com/generation/lojagames/controllers/ProdutoController.java
@@ -2,7 +2,7 @@ package com.generation.lojagames.controllers;
 
 import com.generation.lojagames.model.Produto;
 import com.generation.lojagames.repositories.ProdutoRepository;
-import org.aspectj.weaver.ResolvedPointcutDefinition;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -37,12 +37,12 @@ public class ProdutoController {
     }
 
     @PostMapping
-    public ResponseEntity<Produto> insert(@RequestBody Produto produto){
+    public ResponseEntity<Produto> insert(@Valid @RequestBody Produto produto){
         return ResponseEntity.status(HttpStatus.CREATED).body(produtoRepository.save(produto));
     }
 
     @PutMapping
-    public ResponseEntity<Produto> update(@RequestBody Produto produto){
+    public ResponseEntity<Produto> update(@Valid @RequestBody Produto produto){
         return produtoRepository.findById(produto.getId())
                 .map(obj -> ResponseEntity.ok()
                     .body(produtoRepository.save(produto)))
